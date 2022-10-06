8299a2fd1babe80adde28b4a0840a8534de110f8
OldZack <wang_z1@denison.edu>
2022-10-06 03:18:43 -0400

    Part2 Completed

--- src/MultiThread.java
+++ src/MultiThread.java
@@ -6,15 +6,16 @@ import java.net.Socket;
 
 /*
 This class generate a thread dealing with a client.
-It receives message from client and send the same message back.
+It receives messages from client, checking their validity and send probe messages back.
  */
 class Multithreading extends Thread
 {
     private Socket socket;
     private int portNumber;
     private Integer prob_num;
+    private Integer size;
     private Integer delay;
-    private Integer count;
+
     public Multithreading(Socket s, int port){
         socket = s;
         portNumber = port;
@@ -26,21 +27,61 @@ class Multithreading extends Thread
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         ){
-            String [] inputs = in.readLine().split(" ");
+            // First read the CSP message sent from client.
+            String input = in.readLine();
+            String [] inputs = input.split(" ");
+            // Check message validity.
             if (inputs.length == 5 && inputs[0].equals("s") && (inputs[1].equals("rtt") || inputs[1].equals("tput"))){
-                prob_num = Integer.parseInt(inputs[2]);
-                delay = Integer.parseInt(inputs[4]);
+                try {
+                    prob_num = Integer.parseInt(inputs[2]);
+                    size = Integer.parseInt(inputs[3]);
+                    delay = Integer.parseInt(inputs[4]);
+                // Return 404 if the three parts are not numeric.
+                } catch (NumberFormatException e){
+                    System.out.println("404 ERROR: Invalid Connection Setup Message");
+                    out.println("404 ERROR: Invalid Connection Setup Message");
+                    System.exit(1);
+                }
                 out.println("200 OK: Ready");
             }
             else{
+                System.out.println("404 ERROR: Invalid Connection Setup Message");
                 out.println("404 ERROR: Invalid Connection Setup Message");
                 System.exit(1);
             }
+            // Receive MP message from client for prob_num times.
+            for (int i = 0; i < prob_num; i++){
+                input = in.readLine();
+                inputs = input.split(" ");
+                System.out.println("Receive: " + input);
+                // Check validity of the message.
+                if (inputs.length == 3 && inputs[0].equals("m") && Integer.parseInt(inputs[1]) == i+1 && inputs[2].length() == size){
+                    // Apply delay which simulates the propagation delay.
+                    Thread.sleep(delay);
+                    out.println(input);
+                }
+                else{
+                    System.out.println("404 ERROR: Invalid Measurement Message");
+                    out.println("404 ERROR: Invalid Measurement Message");
+                    System.exit(1);
+                }
+            }
 
+            // Receive CTP message from client and check its validity.
+            if (!in.readLine().equals("t")){
+                System.out.println( "404 ERROR: Invalid Connection Termination Message");
+                out.println("404 ERROR: Invalid Connection Termination Message");
+                System.exit(1);
+            }
+            else{
+                out.println("200 OK: Closing Connection");
+            }
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
                     + portNumber + " or listening for a connection");
             System.out.println(e.getMessage());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     } 
 }
--- src/client.java
+++ src/client.java
@@ -1,22 +1,35 @@
 import java.net.*;
 import java.io.*;
-
+/*
+This class generates a client that accepts host name and port number as
+command line argument.
+It sends message to the server and calculate average RTT/throughput.
+ */
 public class client{
+
+	public static String read(BufferedReader in) throws IOException {
+		String input = in.readLine();
+		while (! in.readLine().endsWith("\n")){
+			input += in.readLine();
+		}
+		return input;
+	}
 
 	public static void main(String [] args) throws IOException{
 		// Check whether the command line argument is the right format.
 		// Print out usage if it is not.
 		if (args.length != 2) {
-			System.err.println(
-					"Usage: java EchoClient <host name> <port number>");
+			System.err.println("Usage: java EchoClient <host name> <port number>");
 			System.exit(1);
 		}
 
-		// Initialize variables that defines message size, probe number, measurement type, server delay
-		// and counter for probe sequence.
+		// Initialize variables that defines message size, probe number and measurement type.
 		Integer size;
 		Integer prob_num;
 		String measure_type;
+		// Initialize the sum of rtts.
+		long rtt_sum = 0;
+		long tput_sum = 0;
 
 		// Assign host name and port number.
 		String hostName = args[0];
@@ -30,38 +43,72 @@ public class client{
 				// Generate a reader of the user input.
 				BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 		) {
+			// Let user define basic parameters.
 			System.out.println("Usage: <MEASUREMENT TYPE> <NUMBER OF PROBES> <MESSAGE SIZE> <SERVER DELAY>");
-			String userInput = stdIn.readLine();
-			String [] inputs = userInput.split(" ");
-			if (inputs.length == 4) {
-				measure_type = inputs[0];
-				prob_num = Integer.parseInt(inputs[1]);
-				size = Integer.parseInt(inputs[2]);
-			}
-			else{
-				throw new Exception();
-			}
-			out.println("s " + userInput);
+			String input = stdIn.readLine();
+			String [] inputs = input.split(" ");
 
-			if (in.readLine() != "200 OK: Ready"){
+			// Send the CSP message to server.
+			out.println("s " + input);
 
+			// Receive message from server.
+			if ((input = in.readLine()).startsWith("404")){
+				System.out.println("Invalid set up message sent.");
+				System.exit(1);
 			}
-			for (Integer i = 1; i < prob_num+1; i++){
-				String payload = new String(new char[size]);
+			System.out.println("Receive: " + input);
+
+			// Assign each value.
+			measure_type = inputs[0];
+			prob_num = Integer.parseInt(inputs[1]);
+			size = Integer.parseInt(inputs[2]);
+
+			// Initialize start time and end time variable that calculates RTT.
+			long start;
+			long end;
+			for (int i = 1; i < prob_num+1; i++){
+				// Generate a string with the size specified by user.
+	 			String payload = new String(new char[size]);
+				start = System.currentTimeMillis();
+				// Send MP message to server.
 				out.println("m " + i + " " + payload);
+				System.out.println("Probe message " + i + " was sent.");
+				input = in.readLine();
+				System.out.println("Receive: " + input);
+				end = System.currentTimeMillis();
+				if (input.startsWith("404")){
+					System.out.println("Invalid measurement message sent.");
+					System.exit(1);
+				};
+				System.out.println("Echo message " + i + " was received.");
+				// Add RTT together to calculate average RTT.
+				rtt_sum += end - start;
+				tput_sum += size/(end - start);
 			}
 
+			// Calculate the average RTT/throughput.
+			System.out.print("Data transmission finishes, ");
+			if (measure_type.equals("rtt")){
+				System.out.println("average RTT: " + rtt_sum/prob_num +"ms.");
+			}
+			else if (measure_type.equals("tput")){
+				System.out.println("average throughput: " + tput_sum/prob_num*0.008 +"Mbps");
+			}
 
-			out.println();
-			System.out.println("Echo message: " + in.readLine());
+			// Send CTP message to server.
+			out.println("t");
+			if ((input = in.readLine()).startsWith("404")){
+				System.out.println( "404 ERROR: Invalid Connection Termination Message");
+				System.exit(1);
+			}
+			System.out.println("Receive: " + input);
+
+
 		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host " + hostName);
 			System.exit(1);
 		} catch (IOException e) {
 			System.err.println("Couldn't get I/O for the connection to " + hostName);
-			System.exit(1);
-		} catch (Exception e) {
-			System.err.println("Wrong input format");
 			System.exit(1);
 		}
 	}
